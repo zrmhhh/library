29c2e0bf9d0a6f71203bcf1b740283d57c8f78fc
zhaoyang <zhaoyang@bizwell.cn>
2018-01-22 16:10:34 +0800

    上传ajax工具、模板解析工具

---
+++ parse_template.js
@@ -0,0 +1,11 @@
+// eg:
+parseTemplate('test666{{orderId}},adsasdf{{no}}zzz', {orderId: 12341234, no: 'asdfsdfsdf'});
+
+// 解析模板
+function parseTemplate(str, obj){
+    for (var key in obj){
+        var reg = new RegExp('{{' + key + '}}');
+        str = str.replace(reg, obj[key]);
+    }
+    return str;
+}
