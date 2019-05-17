7dbd1dac375e8a7da13882b198bdfd5d3e7fba35
zhaoyang <18355403288@163.com>
2019-05-17 15:19:49 +0800

    update 倒计时

---
+++ src/down-date.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param {int} dateTim 
+ */
+function timeElapse(dateTim){
+  var current = Date();
+  var seconds = (Date.parse(current) - dateTim) / 1000;
+  var days = Math.floor(seconds / (3600 * 24));
+  seconds = seconds % (3600 * 24);
+  var hours = Math.floor(seconds / 3600);
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  seconds = seconds % 3600;
+  var minutes = Math.floor(seconds / 60);
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  seconds = seconds % 60;
+  seconds = Math.floor(seconds);
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return days + '天' + hours + '小时' + minutes + '分钟' + seconds + '秒';
+}
