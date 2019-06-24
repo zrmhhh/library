0afb2a976b95c731023713929cd2dd7005525ca1
zhaoyang <18355403288@163.com>
2019-06-24 13:57:23 +0800

    update

--- src/countdown.js
+++ src/countdown.js
@@ -2,21 +2,27 @@
  *
  * @param {int} dateTim 时间戳
  */
-function timeElapse(dateTim) {
+function timeElapse(dateTim, type) {
   if (typeof dateTim !== 'number') return {};
   var days = 0,
     hours = 0,
     minutes = 0,
     seconds = 0;
   var current = Date();
-  seconds = (Date.parse(current) - dateTim) / 1000;
+  var time = seconds = (Date.parse(current) - dateTim) / 1000;
+
   days = Math.floor(seconds / (3600 * 24));
   seconds = seconds % (3600 * 24);
+  if(type === 'hours') seconds = time;
   hours = Math.floor(seconds / 3600);
   seconds = seconds % 3600;
+  if(type === 'minutes') seconds = time;
   minutes = Math.floor(seconds / 60);
   seconds = seconds % 60;
+  if(type === 'minutes') seconds = time;
   seconds = Math.floor(seconds);
+
+  // 添加0
   if (hours < 10) {
     hours = '0' + hours;
   }
