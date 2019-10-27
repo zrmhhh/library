1880346780e42dcbf390388fbe97faff4451299d
zhaoyang <18355403288@163.com>
2019-10-27 19:47:40 +0800

    update

---
+++ lib/handle_file/java/GetFilePath.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileFilter;
+
+public class GetFilePath {
+  public static void main(String[] args) {
+    String path = "./out";
+    File file = new File(path);
+    handleFile(file);
+  }
+
+  private static void handleFile(File file) {
+    File[] fs = file.listFiles();
+    for(File f:fs) {
+      if (f.isDirectory()) {
+        handleFile(f);
+      }
+      if (f.isFile()) {
+        System.out.println(f);
+      }
+    }
+  }
+}
