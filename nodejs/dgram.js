207bebe9963fe524f2ee8bbf079277a5deb6b9ee
zhaoyang <18355403288@163.com>
2019-11-22 23:49:21 +0800

    update

--- nodejs/dgram.js
+++ nodejs/dgram.js
@@ -1,20 +1,20 @@
-var dgram = require ('dgram');
-var server = dgram.createSocket("udp4");
+let dgram = require ('dgram');
+let server = dgram.createSocket("udp4");
 
 server.on ("message", function (msg, rinfo) {
   console.log("server got: " + msg + "\nfrom: " + rinfo.address + ":" + rinfo.port);
 });
 
 server.on("listening", function () {
-  var address =server.address();
+  let address =server.address();
   console.log("server listening: " + address.address + ":" + address.port);
 });
 
 server.bind(7777);
 
 
-var message = Buffer.from("Node.js")
-var client = dgram.createSocket("udp4");
+let message = Buffer.from("Node.js")
+let client = dgram.createSocket("udp4");
 
 client.send(message, 0, message.length, 7777, "localhost", function (err, bytes) {
   client.close();
