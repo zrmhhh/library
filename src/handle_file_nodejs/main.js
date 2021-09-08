ad0eb4509233ce8e8ed3c6b82e41e08896bf6dd7
zhaoyang <980355088@qq.com>
2021-09-08 17:48:52 +0800

    update

--- src/handle_file_nodejs/main.js
+++ src/handle_file_nodejs/main.js
@@ -1,5 +1,5 @@
 global._DIR_ROOT_ = __dirname;
-const filePathListData = require('./_cache/filePathList.json')
+// const filePathListData = require('./_cache/filePathList.json')
 const getFilePath = require('./lib/get-file-path.js');
 
 getFilePath('C:/Users/Administrator/Desktop/github')
