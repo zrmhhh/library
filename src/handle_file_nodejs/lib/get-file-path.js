7775836690836ee4fc35579d01654383ebaa2f41
zhaoyang <980355088@qq.com>
2021-09-08 18:36:48 +0800

    update

--- src/handle_file_nodejs/lib/get-file-path.js
+++ src/handle_file_nodejs/lib/get-file-path.js
@@ -13,57 +13,35 @@ let flatDataArr = []; // 扁平数据
 function main(entryPath){
   let stats = fs.statSync(entryPath); // fs.statSync -> fs.state
   if(stats.isFile()) return console.log('must be directory!');
-  readFileSync(entryPath);
+  readDirectorySync(entryPath);
   writeFile(`${_DIR_ROOT_}/_cache/filePathList.json`, flatDataArr);
 }
 
 // sync get list
-function readFileSync(dirPath) {
+function readDirectorySync(dirPath) {
   let files = fs.readdirSync(dirPath);
-  files.forEach(async filename => {
+  files.forEach(filename => {
     let rootPath = path.resolve(path.join(dirPath, filename));
     let stats = fs.statSync(rootPath);
-    //是文件
+
     if (stats.isFile()) {
-      // hash = await generateMD5(rootPath);
-      let dataJson = {
-        name: filename,
-        path: rootPath,
-        // hash,
-        size: stats.size / 1024 / 1024, // MB
-        size_bit: stats.size // bit
-      };
-      flatDataArr.push(dataJson); // 填充扁平数据
+      readFileSync(filename, rootPath, stats)
     } else if (stats.isDirectory()) {
-      readFileSync(rootPath);
+      readDirectorySync(rootPath);
     }
   });
 }
-// async get list
-function readFile(dirPath) {
-  fs.readdir(dirPath, function(err, files) { // get all file list
-    if (err) throw err;
 
-    files.forEach(filename => {
-      let fullFileName = path.join(dirPath, filename);
-      fs.stat(fullFileName, function(err, stats) {
-        if (err) throw err;
-        //是文件
-        if (stats.isFile()) {
-          let dataJson = {
-            name: filename,
-            path: fullFileName,
-            size: stats.size
-          };
-          // flatDataArr.push(dataJson); // 填充扁平数据
-          // writeFile('./out/filePathList.json', flatDataArr); // 待优化
-        } else if (stats.isDirectory()) {
-          readFile(fullFileName);
-        }
-      });
-    });
-  });
+async function readFileSync(filename, rootPath, stats) {
+  // hash = await generateMD5(rootPath);
+  let dataJson = {
+    name: filename,
+    path: rootPath,
+    // hash,
+    size: stats.size / 1024 / 1024, // MB
+    size_bit: stats.size // bit
+  };
+  flatDataArr.push(dataJson); // 填充扁平数据
 }
 
-// main('../../src');
 module.exports = main;
